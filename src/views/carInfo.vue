f90d0e9ffdce2b6040d45c2435fe031f56f00318
TSDD1111 <1016197899@qq.com>
2021-06-17 20:08:50 +0800

    BUPT二手车交易平台车辆信息、卖车界面完善

--- src/views/carInfo.vue
+++ src/views/carInfo.vue
@@ -5,7 +5,7 @@
       </div>
     </el-col>
   </el-row>
-  <el-row gutter="11">
+  <el-row gutter="111">
     <el-col :span="12"><div class="grid-content bg-purple">
       <img id="picture" />
     </div></el-col>
@@ -53,6 +53,7 @@
     </div></el-col>
   </el-row>
 
+
   <el-row gutter="1111">
     <el-col :span="24"><div class="grid-content bg-purple-dark">
       <h1 class="baseinfo">
@@ -87,6 +88,10 @@
         <el-table-column
             prop="notRepairedDamage"
             label="是否有未修复的损坏">
+        </el-table-column>
+        <el-table-column
+            prop="userTelnum"
+            label="卖家电话">
         </el-table-column>
       </el-table>
 
@@ -138,15 +143,16 @@
   </el-dialog>
 </template>
 
+import axios;
 <script>
 // eslint-disable-next-line no-unused-vars
 import {getCarInfo, submitOrder} from "../http/api";
-import {getCarComment,insertComment,insertOrder} from "../http/api";
+import {getCarComment,insertComment,insertOrder,getUserInfo} from "../http/api";
 import {ref,reactive} from "vue";
 import router from "../router";
 
 export default {
-  name : 'CarInfo',
+  name : 'carinfo',
   setup() {
     let car_information =reactive([{
       name:"奔驰S500",
@@ -162,7 +168,8 @@ export default {
       creatDate:0,
       prePrice:10000,
       sellerID:0,
-      carDescribe:0
+      carDescribe:0,
+      userTelnum:0
     }])
     let carImgs = require("../assets/logo.png")
     let input_comment = ref(null)
@@ -170,7 +177,7 @@ export default {
     let buycar_button = ref(false)
     let submitcomment_button = ref(false)
     let carid = router.currentRoute.value.query.carId
-    let userid = 0
+    let userid = null
     let carcomments  = reactive([])
     let dialog1Visible = ref(false)
     let diglog1Text = ref("点击提交评论")
@@ -181,25 +188,26 @@ export default {
 
     getCarInfo(carid).then(res=>{
       document.getElementById("picture").src = res.carImages[0]
-      //carImgs = require(res.carImgs[0].carImg)
       car_information[0].name = res.carInfo.name
       car_information[0].brand = res.carInfo.brand
       car_information[0].prePrice = res.carInfo.prePrice
       car_information[0].kilometer = res.carInfo.kilometer
       car_information[0].power = res.carInfo.power
       car_information[0].fuelType = res.carInfo.fuelType
+      car_information[0].userTelnum = res.owner.userTelnum
+
       if(res.carInfo.gearbox === 0){
         car_information[0].gearbox = "手动"
       }else{
         car_information[0].gearbox = "自动"
       }
-      if(res.carInfo.fuelType === 0){
+      if(res.carInfo.fueltype === 0){
         car_information[0].fuelType = "汽油"
       }
-      else if(res.carInfo.fuelType == 1) {
+      else if(res.carInfo.fueltype === 1) {
         car_information[0].fuelType = "柴油"
       }
-      else if(res.carInfo.fuelType == 2) {
+      else if(res.carInfo.fueltype === 2) {
         car_information[0].fuelType = "电动"
       }
       else{
@@ -221,54 +229,82 @@ export default {
       }
     })
     function submitComment() {
-      if (submitcomment_button.value == false&&input_comment.value!=null) {
-        submitcomment_button.value = true
-        successbutton.value = true
-        let data = {
-          carId: carid,
-          userId: userid,
-          commentDetails: input_comment.value
+      getUserInfo().then(res=>{
+        if(res == ""){
+          userid= null
+        }else{
+          userid= res.userId
         }
-        insertComment(data).then(res => {
-          if(res=="") {
-            diglog2Text.value = "提交成功"
-            dialog2Visible.value = true
-            input_comment.value = null
-            submitcomment_button.value = false
-            successbutton.value = false
-          }else {
-            diglog2Text.value = "提交未成功"
-            dialog2Visible.value = true
-            input_comment.value = null
-            successbutton.value = false
+      })
+      console.log(userid)
+      if(userid!=null) {
+        if (submitcomment_button.value == false && input_comment.value != null) {
+          submitcomment_button.value = true
+          successbutton.value = true
+          let data = {
+            carId: carid,
+            userId: userid,
+            commentDetails: input_comment.value
           }
-        })
+
+          insertComment(data).then(res => {
+            if (res == "") {
+              diglog2Text.value = "提交成功"
+              dialog2Visible.value = true
+              input_comment.value = null
+              submitcomment_button.value = false
+              successbutton.value = false
+            } else {
+              diglog2Text.value = "提交未成功"
+              dialog2Visible.value = true
+              input_comment.value = null
+              successbutton.value = false
+            }
+          })
+        } else {
+          diglog2Text.value = "评论不能为空"
+          dialog2Visible.value = true
+        }
       }
       else {
-        diglog2Text.value = "评论不能为空"
+        diglog2Text.value = "请先登录"
         dialog2Visible.value = true
       }
 
     }
-    function buyCar(){
-      if(buycar_button.value == false) {
-        let order = {
-          buyerId: userid,
-          carId: carid,
-          sellerId: car_information[0].prePrice,
-          tradePrice: car_information[0].sellerID
+    function buyCar() {
+      //此处判断是否登录
+      getUserInfo().then(res=>{
+        if(res == ""){
+          userid= null
+        }else{
+          userid= res.userId
         }
-        text1.value = "提交中"
-        insertOrder(order).then(res =>{
-          if(res == "") {
-            text1.value = "已提交"
-            buycar_button.value = true
-          }else {
-            dialog1Visible.value = true
-            text1.value = "我要买车"
-            diglog1Text.value = "提交失败"
+      })
+      if (userid != null) {
+        if (buycar_button.value == false) {
+          let order = {
+            buyerId: userid,
+            carId: carid,
+            sellerId: car_information[0].prePrice,
+            tradePrice: car_information[0].sellerID
           }
-        })
+          text1.value = "提交中"
+          insertOrder(order).then(res => {
+            if (res == "") {
+              text1.value = "已提交"
+              buycar_button.value = true
+            } else {
+              dialog1Visible.value = true
+              text1.value = "我要买车"
+              diglog1Text.value = "提交失败"
+            }
+          })
+        }
+      }
+      else{
+        dialog1Visible.value = true
+        diglog1Text.value = "请先登录"
       }
     }
 
--- src/views/sellCars.vue
+++ src/views/sellCars.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="big">
-    <button class="salecar" @click="dialogTableVisible = true">我要卖车</button>
+    <button class="salecar" @click="logincheck()">我要卖车</button>
     <el-dialog title="我要卖车" v-model="dialogTableVisible">
       <el-form :model="form">
         <el-form-item label="品牌" :label-width="formLabelWidth">
@@ -8,9 +8,25 @@
             <el-option v-for="i in carbrands" :key="i" :label="i.brand" :value="i.brand" @click="requestCarSeries(i.brand)"></el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="日期" :label-width="formLabelWidth">
+          <el-date-picker
+              v-model="form.regdate"
+              type="date"
+              placeholder="Pick a date"
+              :default-value="new Date(2010, 9, 1)"
+          >
+          </el-date-picker>
+        </el-form-item>
+
         <el-form-item label="型号" :label-width="formLabelWidth">
           <el-select v-model="form.name">
             <el-option v-for="i in carinfo[0]" :key="i" :label="i.carSeries" :value="i.carSeries" @click="updateForm(i)"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="是否有未维修的损坏" :label-width="formLabelWidth">
+          <el-select v-model="form.isnotrepair" >
+            <el-option  label="是" :value="0"></el-option>
+            <el-option  label="否" :value="1"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="行驶里程(公里)"  :label-width="formLabelWidth" prop="kilometer" :rules="[
@@ -19,12 +35,30 @@
     ]">
           <el-input type="kilometer" v-model.number="form.kilometer" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="是否有未维修的损坏" :label-width="formLabelWidth">
-          <el-select v-model="form.notrepaireddamage" >
-            <el-option  label="是" :value="0"></el-option>
-            <el-option  label="否" :value="1"></el-option>
-          </el-select>
+        <el-form-item label="价格(万)"  :label-width="formLabelWidth" prop="price" :rules="[
+      { required: true, message: '价格不能为空'},
+      { type: 'number', min:1,max:1000,message: '价格必须为数字值,且要符合大小'}
+    ]">
+          <el-input type="price" v-model.number="form.price" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="描述"  :label-width="formLabelWidth" prop="carDescribe" :rules="[
+      { required: true, message: '描述不能为空'},
+    ]">
+          <el-input type="carDescribe" v-model.number="form.carDescribe" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="车辆图片" :label-width="formLabelWidth">
+          <el-upload
+              class="upload-demo"
+              action="http://8.140.2.249//car/oss/uploadFile"
+              :on-success="Upload"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+            <template #tip>
+              <div class="el-upload__tip">只能上传 jpg/png 文件，且不超过 500kb</div>
+            </template>
+          </el-upload>
+        </el-form-item>
+
         <el-form-item  :label-width="formLabelWidth">
           <el-button type="primary" @click="submitForm()" :loading="submit_button_load">提交</el-button>
         </el-form-item>
@@ -50,7 +84,7 @@ import axios;
 import {reactive, ref} from "vue";
 import {isNumber} from "element-plus/es/utils/util";
 // eslint-disable-next-line no-unused-vars
-import {getCarBrand,getCarSeries,insertCar} from "../http/api";
+import {getCarBrand, getCarSeries, getUserInfo, insertCar, submmitUpload} from "../http/api";
 
 export default {
   name: 'sellcars',
@@ -59,23 +93,18 @@ export default {
     let dialogTableVisible = ref(false)
     let formLabelWidth =  '120px'
     let form = reactive({
-      sellerId:0,
-      carId:0,
-      carDescribe:"啊啊啊啊",
-      name:"",
-      regdate : 0,
-      model : 0,
-      brand : "",
-      bodytype:0,
-      fueltype:0,
-      gearbox:0,
-      power:0,
+      brand:null,
+      carDescribe:null,
+      images:null,
+      isnotrepair:null,
       kilometer:null,
-      notrepaireddamage:null,
-      regioncode:0,
-      seller:0,
-      offertype:1,
-      prePrice:0
+      model:null,
+      name:null,
+      power:null,
+      price:null,
+      regdate:null,
+      region:"北京",
+      sellerId:1
     })
     let carbrands = reactive([])
     let carnames = reactive([])
@@ -83,15 +112,37 @@ export default {
     let diglogVisible = ref(false)
     let diglogText = ref(null)
     let submit_button_load = ref(false)
-
     getCarBrand().then(
         res=>{
           for(var i =0;i<res.length;i++){
             carbrands.push({brand:res[i]})
           }
         })
+    function logincheck(){
+      //此处判断是否登录
+      getUserInfo().then(res=>{
+        if(res == ""){
+          form.sellerId= null
+        }else{
+          form.sellerId = res.userId
+        }
+      })
+      if(form.sellerId==null) {
+        diglogVisible.value = true
+        diglogText.value = "请先登录"
+      }else{
+        dialogTableVisible.value = true
+      }
+    }
+
     function submitForm() {
-      if (isNumber(form.kilometer)&&form.brand!=""&&form.notrepaireddamage!=null&&form.name!="") {
+      if (isNumber(form.price)&&isNumber(form.kilometer)&&form.price>0
+          &&form.kilometer>0&&form.brand!=""&&form.isnotrepair!=null
+          &&form.name!=""&&form.carDescribe!=null&&form.price!=null
+          &&form.regdate!=null&&form.images!=null
+      )
+      {
+        console.log(form)
         insertCar(form).then(res=>{
           if(res == "") {
             dialogTableVisible.value = false
@@ -101,13 +152,14 @@ export default {
         })
       }
     }
+
     function updateForm(i){
       form.brand = i.brand
       form.name = i.carSeries
-      form.gearbox = i.gearbox
-      form.fuelType = i.fuelType
-      form.bodyType = i.bodytype
+      form.model = i.bodytype
+      form.power = "1.5L"
     }
+
     function  requestCarSeries(brand){
       getCarSeries(brand).then(res=>{
         carinfo.splice(0)
@@ -115,6 +167,14 @@ export default {
         form.name = ""
       })
     }
+
+    //upload images,res is url of images
+    function Upload(res) {
+      console.log(res)
+      form.images = []
+      form.images.push(res)
+    }
+
     return {
       dialogTableVisible,
       form,
@@ -128,6 +188,8 @@ export default {
       diglogVisible,
       submit_button_load,
       diglogText,
+      Upload,
+      logincheck
     }
   },
 }
